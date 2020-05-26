f23eeee2e27bef8f4a7dbbe34af793e305234f85
dr-Luka <63805178+dr-Luka@users.noreply.github.com>
2020-05-26 21:27:51 +0200

    Added Final Product
    Programming Foundations, MA 2 delivery

---
+++ script.js
@@ -0,0 +1,43 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log(petArray[1]);
+
+// question 3
+petArray.push("sheep");
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject = {
+  name: "Kitty",
+  colour: "Rainbow",
+  age: 100,
+};
+
+// question 6
+console.log(catObject.age);
+
+// question 7
+var catArray = [
+  {
+    name: "Kitty",
+    colour: "Purple",
+    age: 100,
+  },
+];
+
+// question 8
+for (var i = 0; i < catArray.length; i += 1) {
+  console.log(catArray[i]);
+}
+
+// question 9
+function logToConsole(word) {
+  console.log(word);
+}
+
+// question 10
+logToConsole("donkey");
